852a0e265ac3b0721a85b92c8afdc7874f2919d6
Jonathan Battisti <jonathan.battisti.1@gmail.com>
2023-01-02 05:29:11 +0000

    Add d IsDevisableBySeven

--- src/001/exercise001.go
+++ src/001/exercise001.go
@@ -7,3 +7,7 @@ import (
 func main() {
 	fmt.Println("Hello world")
 }
+
+func IsDevisableBySeven(number int) bool {
+	return number%7 == 0
+}
--- src/001/exercise001_test.go
+++ src/001/exercise001_test.go
@@ -4,8 +4,23 @@ import (
 	"testing"
 )
 
-func TestDevisable(t *testing.T) {
-	if true {
-		t.Errorf("nothing")
+func TestIsDevisableBySeven(t *testing.T) {
+	inputs := []struct {
+		number   int
+		expected bool
+	}{
+		{number: 0, expected: true},
+		{number: 1, expected: false},
+		{number: 2, expected: false},
+		{number: 7, expected: true},
+		{number: 2000, expected: false},
+		{number: 2009, expected: true},
+	}
+
+	for _, input := range inputs {
+		actual := IsDevisableBySeven(input.number)
+		if input.expected != actual {
+			t.Errorf("\"DevisableBySeven('%v')\" failed, expected -> %v, got -> %v", input.number, input.expected, actual)
+		}
 	}
 }
