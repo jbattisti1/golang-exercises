8ed5b0449b037234bfe665f16ea1a2c9a44284b7
Jonathan Battisti <jonathan.battisti.1@gmail.com>
2023-01-02 05:09:25 +0000

    Init exercise001

---
+++ src/001/exercise001_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestDevisable(t *testing.T) {
+	if true {
+		t.Errorf("nothing")
+	}
+}
---
+++ src/001/go.mod
@@ -0,0 +1,3 @@
+module github.com/jbattisti1/golang-exercises/src/001
+
+go 1.19
