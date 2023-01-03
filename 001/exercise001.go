4bd100e1ef8a2257abe188214ff00b8a9ee0a81b
Jonathan Battisti <jonathan.battisti.1@gmail.com>
2023-01-03 03:56:27 +0000

    Implement isNotMultipleOfFive

--- 001/exercise001.go
+++ 001/exercise001.go
@@ -3,3 +3,11 @@ package exercise001
 func isDevisableBySeven(number int) bool {
 	return number%7 == 0
 }
+
+func isNotMultipleOfFive(number int) bool {
+	lastDigit := number % 10
+	if lastDigit == 0 || lastDigit == 5 {
+		return false
+	}
+	return true
+}
--- 001/exercise001_test.go
+++ 001/exercise001_test.go
@@ -24,3 +24,27 @@ func TestIsDevisableBySeven(t *testing.T) {
 		}
 	}
 }
+
+func TestIsNotMutipleOfFive(t *testing.T) {
+	inputs := []struct {
+		number   int
+		expected bool
+	}{
+		{number: 0, expected: false},
+		{number: 1, expected: true},
+		{number: 5, expected: false},
+		{number: 7, expected: true},
+		{number: 10, expected: false},
+		{number: 100, expected: false},
+		{number: 275, expected: false},
+		{number: 278, expected: true},
+		{number: 2000, expected: false},
+	}
+
+	for _, input := range inputs {
+		actual := isNotMultipleOfFive(input.number)
+		if input.expected != actual {
+			t.Errorf("\"isNotMultipleOfFive(%v)\" failed, expected -> %v, got -> %v", input.number, input.expected, actual)
+		}
+	}
+}
